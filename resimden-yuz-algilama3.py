88e4e4e2662e379fb80f6d321fc44ce20b87bb89
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-10-31 23:31:48 +0300

    yüz algılama işlemi tamamlandı.

---
+++ resimden-yuz-algilama3.py
@@ -0,0 +1,16 @@
+import cv2
+
+img = cv2.imread("face.png")
+
+face_cascade = cv2.CascadeClassifier("frontalface.xml")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 7)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+
+cv2.imshow("img", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
