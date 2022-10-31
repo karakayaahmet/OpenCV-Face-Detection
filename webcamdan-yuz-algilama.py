d086ee2bd9fde759150b2966aec76a4b9a803644
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-10-31 23:56:44 +0300

    webcam üzerinden yüz algılama tamamlandı.

--- webcamdan-yuz-algilama.py
+++ webcamdan-yuz-algilama.py
@@ -1,14 +1,12 @@
 import cv2
 
-cap = cv2.VideoCapture("faces.mp4")
+cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier("frontalface.xml")
 
 while True:
     ret, frame = cap.read()
-
-    if ret is False:
-        break
+    frame = cv2.flip(frame, 1)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -17,9 +15,9 @@ while True:
     for (x,y,w,h) in faces:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
 
-    cv2.imshow("img", frame)
+    cv2.imshow("webcam", frame)
 
-    if cv2.waitKey(20) & 0xFF == ord("q"):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
